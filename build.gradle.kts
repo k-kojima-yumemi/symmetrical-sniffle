4535f6d3a6dca11ff3dbcf28c1dcbd6bdcc85a5f
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-05-03 16:51:32 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "jp.co.yumemi.koma"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
